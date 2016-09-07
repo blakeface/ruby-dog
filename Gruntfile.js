248e1a3fb652a2d60e7e372f1ceb23a150908940
Blake Netzeband <blake.netzeband@gmail.com>
2016-09-07 11:05:27 -0600

    updated readme. successfully configured grunt and phaser

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,38 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    connect: {
-      server: {
-        options: {
-          port: 8080,
-        }
-      }
-    }
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: 'src/game.js',
+        dest: 'dist/game.min.js',
       },
     },
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js'],
       options: {
+        esversion: 6,
         globals: {
           jQuery: true,
           console: true,
-          module: true
+          module: true,
         },
       },
     },
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['jshint'],
+      tasks: ['jshint', 'uglify'],
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: 'dist',
+          keepalive: true,
+        },
+      },
     },
   });
 
@@ -39,5 +42,4 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint', 'uglify', 'connect']);
-
 };
--- readme.md
+++ readme.md
@@ -1,7 +1,10 @@
 #Ruby Dog   
-
-Packaged with Grunt   
+Help the ever-so flatulent Ruby Dog avoid the health-freaks and score some peanut butter. Careful though, if she gets too spooked she'll puke.
 
 ###To install and run:   
-    $ npm i
+    $ npm i  
     $ grunt
+
+###Specs:
+* Grunt
+* Phaser.io
