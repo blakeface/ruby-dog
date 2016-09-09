05c9bed12975e197d15c5bc984bf0e9ade7efbe1
Blake Netzeband <blake.netzeband@gmail.com>
2016-09-08 22:37:58 -0600

    preloaded tilemap - does not render

--- src/game.js
+++ src/game.js
@@ -1,12 +1,15 @@
 (function () {
-  const game = new Phaser.Game(800, 600, Phaser.AUTO, 'root', { preload: preload, create: create, update: update });
+  const game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
   function preload() {
+    game.load.tilemap('map', 'assets/map.json', Phaser.Tilemap.TILED_JSON);
   }
 
   function create() {
+
   }
 
   function update() {
+    // this.physics.arcade.collide(this.ruby, this.layer);
   }
 })();
